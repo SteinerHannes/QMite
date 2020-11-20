49e791dc131bc46268c7c92e163402c84e3369ae
Hannes Steiner <hannes.steiner@web.de>
2020-11-20 09:28:25 +0100

    Initial Commit

---
+++ Shared/QMiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  QMiteApp.swift
+//  Shared
+//
+//  Created by Hannes Steiner on 20.11.20.
+//
+
+import SwiftUI
+
+@main
+struct QMiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
