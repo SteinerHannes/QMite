30e0bd438d2a8a30cd03f16faa7bc10cb293b131
Hannes Steiner <hannes.steiner@web.de>
2020-11-20 17:09:33 +0100

    Sidebar iPad / mac

--- Shared/Navigation/SideBar.swift
+++ Shared/Navigation/SideBar.swift
@@ -11,6 +11,20 @@ struct SideBar: View {
     @State private var selectedNavigationItem: NavigationItem = .recordTime 
     
     var body: some View {
+        NavigationView {
+            #if os(iOS)
+            sidebar
+                .navigationTitle("Navigation")
+            #else
+            sidebar
+                .frame(minWidth: 100, idealWidth: 150, maxWidth: 200, maxHeight: .infinity)
+                .navigationTitle("Navigation")
+            #endif
+            Text("Select a Section")
+        }
+    }
+    
+    var sidebar: some View {
         List {
             ForEach(NavigationItem.allCases) { item in
                 NavigationLink(item.userFacingString, destination: item.view)
