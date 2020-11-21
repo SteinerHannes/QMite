4270bd040296fb22a44d17146811cc898bc12cdf
Hannes Steiner <hannes.steiner@web.de>
2020-11-21 11:35:45 +0100

    Keychain + onAppear

--- Shared/AppCore.swift
+++ Shared/AppCore.swift
@@ -9,18 +9,38 @@ import Foundation
 import ComposableArchitecture
 
 struct AppState: Equatable {
+    var isAPIKeyAvailable: Bool = false
 }
 
 enum AppAction: Equatable {
+    case onAppear
 }
 
 struct AppEnvironment {
     var mainQueue: AnySchedulerOf<DispatchQueue>
+
+    private enum Keys: String {
+        case miteApiKey
+    }
+
+    func getMiteAPIKey() -> String? {
+        return KeychainWrapper.standard.string(forKey: Keys.miteApiKey.rawValue)
+    }
+
+    func setMiteAPIKey(for value: String) {
+        KeychainWrapper.standard.set(value, forKey: Keys.miteApiKey.rawValue)
+    }
 }
 
-private let _appReducer = Reducer<AppState, AppAction, AppEnvironment> { _, action, _ in
+private let _appReducer = Reducer<AppState, AppAction, AppEnvironment> { state, action, env in
     switch action {
-        @unknown default:
-        return .none
+        case .onAppear:
+            if env.getMiteAPIKey() == nil {
+                state.isAPIKeyAvailable = false
+            } else {
+                state.isAPIKeyAvailable = true
+            }
+
+            return .none
     }
 }
